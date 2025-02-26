95f7d1a964490fa52d802ff91a642971cbbc82ef
EC2 Default User <ec2-user@ip-172-31-38-253.ec2.internal>
2025-02-26 04:07:44 +0000

    test task sh

--- k8s_yaml/task2.sh
+++ k8s_yaml/task2.sh
@@ -3,6 +3,7 @@
 
 kubectl apply -f mysql_pod.yaml
 kubectl apply -f mysql_service.yaml
+sleep 5
 kubectl apply -f employee_pod.yaml
 sleep 10
 nohup kubectl port-forward employees -n clo835 8080:8080 > /dev/null 2>&1 &
--- k8s_yaml/task6.sh
+++ k8s_yaml/task6.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-kubectl rollout history deployment.apps/employees -n clo835 
-kubectl apply -f employee_deployment.yaml --record 
-kubectl rollout status deployment.apps/employees -n clo835 
-kubectl rollout history deployment.apps/employees -n clo835 
+kubectl rollout history deployment.apps/employees-deployment -n clo835
+kubectl apply -f employee_deployment.yaml --record
+kubectl rollout status deployment.apps/employees-deployment -n clo835
+kubectl rollout history deployment.apps/employees-deployment -n clo835
+kubectl describe deployment.apps/employees-deployment -n clo835
