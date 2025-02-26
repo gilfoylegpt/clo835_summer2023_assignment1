9108b68ce860fa16f41c4584e1ff033e3b27745c
gilfoylegpt <fuhaojie930@gmail.com>
2025-02-25 19:15:06 -0500

    add k8s yaml

---
+++ k8s_yaml/secrets.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ kubectl create secret docker-registry regcred \
+  --docker-server=138098912972.dkr.ecr.us-east-1.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password=$(aws ecr get-login-password --region us-east-1) \
+  -n clo835
+  
